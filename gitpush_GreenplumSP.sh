538ec28b120a106ea3f1c8f6aa816fec5ebc54b6
Sanghee Lee <bigcrow79@gmail.com>
2021-02-26 09:05:25 +0900

    first commit

---
+++ gitpush_GreenplumSP.sh
@@ -0,0 +1,18 @@
+# git init
+#git add *
+#git commit -m "first commit"
+#git remote add origin https://github.com/gpdbkr/GreenplumSA.git
+#git push -u origin master
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/gpdbkr/GreenplumSA.git
+# git push -u origin master
+
+#echo "# GreenplumSP" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git remote add origin https://github.com/gpdbkr/GreenplumSP.git
+git push -u origin master
+
+#git remote add origin https://github.com/gpdbkr/GreenplumSP.git
+#git push -u origin master
